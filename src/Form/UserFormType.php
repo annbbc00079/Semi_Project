308ba30a6efd2fab8328b98ffd23d52ce7bdd521
Your Name <you@example.com>
2023-07-26 18:06:58 +0700

    semic

---
+++ src/Form/UserFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username',TextType::class)
+     
+            ->add('password',Password::class)
+            ->add('first_name',TextType::class)
+            ->add('last_name',TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
