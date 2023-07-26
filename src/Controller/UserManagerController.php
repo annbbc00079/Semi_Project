8b0323811050255a41f61a365f5562747948d826
Your Name <you@example.com>
2023-07-27 02:53:11 +0700

    newcm

--- src/Controller/UserManagerController.php
+++ src/Controller/UserManagerController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class UserManagerController extends AbstractController
 {
-    #[Route('/user/manager', name: 'app_user_manager')]
-    public function index(): Response
+    public function __construct(private UrlGeneratorInterface $url)
+    {      
+    }
+    #[Route('/user/manager/add', name: 'app_user_manager_add')]
+    public function usermanager(Request $req, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $em): Response
     {
-        return $this->render('user_manager/index.html.twig', [
-            'controller_name' => 'UserManagerController',
+        $u = new User();
+        $form = $this->createForm(UserFormType::class, $u);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+            $data = $form->getData();{
+            // encode the plain password
+            $u->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $u,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $u = setRoles(["ROLE_ADMIN"]);
+            $em->persist($u);
+            $em->flush();
+            // do anything else you need here, like send an email
+
+            return new RedirectResponse($this->urlGenerator->generate('app_user_manager'));
+        }
+
+        return $this->render('user_manager/list.html.twig', [
+            'u_form' => $form->createView(),
         ]);
     }
+    #[Route('/user/manager', name: 'app_user_manager')]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $query = $em->createQuery('SELECT u FROM App\Entity\User u');
+        $lUser = $query->getResult();
+        
+        return $this->render('user_manager/index.html.twig', [
+            'data' => $lUser
+        ]);
+    }
+    
 }
